e347cc393d04a0b1a1d873a46c05f917586237c0
MartinKamenov <martinkamenov10@gmail.com>
2019-06-06 16:00:53 +0300

    Use new db for application

--- src/database/connectorOriginal.js
+++ src/database/connectorOriginal.js
@@ -1,6 +1,8 @@
 const Database = require('./mongodb');
-const username = process.env.username || require('../../../credentials/credentialManager').username;
-const password = process.env.password || require('../../../credentials/credentialManager').password;
+const username = process.env.githubAnalyzatorUser || 
+    require('../../../credentials/credentialManager').githubAnalyzatorUser;
+const password = process.env.githubAnalyzatorPassword || 
+    require('../../../credentials/credentialManager').githubAnalyzatorPassword;
 const connectionstring = `mongodb://${username}:${password}@ds233367-a0.mlab.com:33367,ds233367-a1.mlab.com:33367/githubanalyzator?replicaSet=rs-ds233367`;
 const database = new Database(connectionstring);
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const database = require('./database/connector');
 const originalDatabase = require('./database/connectorOriginal');
 const UserRepository = require('./models/repositories/UserRepository');
 const RepoRepository = require('./models/repositories/RepoRepository');
-const userRepository = new UserRepository(database, 'users');
 const originalUserRepository = new UserRepository(originalDatabase, 'users');
-const repoRepository = new RepoRepository(database, 'repositories');
 const originalRepoRepository = new RepoRepository(originalDatabase, 'repositories');
 const errorRoute = require('./routers/error/error-route');
 const githubRoute = require('./routers/github/github-route');
@@ -21,28 +18,13 @@ const start = (setupConfiguration) => {
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
-    githubRoute(app, userRepository, repoRepository);
+    githubRoute(app, originalUserRepository, originalRepoRepository);
     errorRoute(app);
 
     app.listen(setupConfiguration.port,
         setupConfiguration.startCallback());
 
-    //automaticIndexator.start(userRepository);
-};
-
-const copyDb = async () => {
-    const originalUsers = await originalUserRepository.getAllUsers();
-    let users = await userRepository.getAllUsers();
-    users = users.reverse();
-    let userIndex = 1;
-    users.forEach(async (user) => {
-        if(originalUsers.find((u) => u.username === user.username)) {
-            return;
-        }
-        await originalUserRepository.addUser(user);
-        console.log(user.username + 'was added ' + (userIndex++) + '/' + users.length);
-    });    
+    //automaticIndexator.start(originalUserRepository);
 };
 
 start(setupObject);
-copyDb();
