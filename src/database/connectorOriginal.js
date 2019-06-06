12a6f6915d73cf00fb16350b03db5279d090a585
MartinKamenov <martinkamenov10@gmail.com>
2019-06-06 15:13:19 +0300

    Import credentials from outside

--- src/database/connectorOriginal.js
+++ src/database/connectorOriginal.js
@@ -1,8 +1,6 @@
 const Database = require('./mongodb');
-/*const username = process.env.username;
-const password = process.env.password;*/
-const username = 'mkuser';
-const password = 'blu3isth3color';
+const username = process.env.username || require('../../../credentials/credentialManager').username;
+const password = process.env.password || require('../../../credentials/credentialManager').password;
 const connectionstring = `mongodb://${username}:${password}@ds233367-a0.mlab.com:33367,ds233367-a1.mlab.com:33367/githubanalyzator?replicaSet=rs-ds233367`;
 const database = new Database(connectionstring);
 
